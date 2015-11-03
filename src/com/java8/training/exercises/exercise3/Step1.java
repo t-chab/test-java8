0b851e97982defe308de04d73319f668e46b874f
tchabaud <tom@chabs.name>
2015-11-03 15:18:47 +0100

    Added exercise 3

---
+++ src/com/java8/training/exercises/exercise3/Step1.java
@@ -0,0 +1,66 @@
+package com.java8.training.exercises.exercise3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created on 03/11/2015.
+ */
+class Step1 {
+    private final static String SEPARATOR = "------------------------------------------------------";
+
+    public static String searchFor(Stream<String> st, String str) {
+        return st.map(String::toUpperCase)
+                .filter(s -> s.length() <= 4)
+                .filter(t -> t.contains(str))
+                .peek(System.out::println)
+                .findFirst().orElse("");
+    }
+
+    public static void main(String[] args) {
+        List<String> firstnames = Arrays.asList("Lisa", "Bart", "Maggie", "Marge", "Homer", "Apu", "Mandula");
+
+        firstnames.stream().forEach(firstname -> System.out.printf("%s\n", firstname));
+        firstnames.stream().forEach(System.out::println);
+        List<String> result = firstnames.stream()
+                .filter(firstname -> firstname.startsWith("M"))
+                .peek(firstname -> System.out.printf("%s --\n", firstname))
+                .collect(Collectors.toList());
+        System.out.println(result);
+
+        System.out.println(SEPARATOR);
+
+        List<String> r1 = firstnames.stream().map(firstname -> firstname.concat("!")).collect(Collectors.toList());
+        System.out.println(r1);
+        List<String> r2 = firstnames.stream().map(firstname -> firstname.replace("ic", "ik")).collect(Collectors.toList());
+        System.out.println(r2);
+        List<String> r3 = firstnames.stream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(r3);
+
+        System.out.println(SEPARATOR);
+
+        String letter = "b";
+
+        long c1 = firstnames.stream().filter(s -> s.length() <= 4).peek(System.out::println).count();
+        System.out.println("Names lower than 4: " + c1);
+        long c2 = firstnames.stream().filter(s -> s.toLowerCase().contains(letter)).peek(System.out::println).count();
+        System.out.println("Names that contains a b: " + c2);
+        long c3 = firstnames.stream().filter(s -> (s.length() % 2) == 0).peek(System.out::println).count();
+        System.out.println("Names that length is multiple of 2: " + c3);
+
+        System.out.println(SEPARATOR);
+
+        firstnames.stream().map(String::toUpperCase)
+                .filter(s -> s.length() <= 4)
+                .filter(t -> t.contains(letter))
+                .peek(System.out::println)
+                .findFirst();
+
+        System.out.println(SEPARATOR);
+
+        System.out.println(searchFor(firstnames.stream(), letter));
+        System.out.println(searchFor(firstnames.stream(), "E"));
+    }
+}
---
+++ src/com/java8/training/exercises/exercise3/Step2.java
@@ -0,0 +1,52 @@
+package com.java8.training.exercises.exercise3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by 03/11/2015.
+ */
+class StreamUtils {
+    public static List<Double> randomNumberList(int size) {
+        return Stream.generate(Math::random)
+                .limit(size)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> orderedNumberList(int start, int step, int size) {
+        return Stream.iterate(start, (i) -> i + step)
+                .limit(size)
+                .collect(Collectors.toList());
+    }
+}
+
+class Step2 {
+
+    public static void main(String[] args) {
+        String[] firstnames = {"Homer", "Lisa", "Marge", "Bart"};
+
+        String result = Arrays.stream(firstnames).reduce("", (s1, s2) -> s1.toUpperCase() + " " + s2.toUpperCase());
+        System.out.println(result);
+
+        String result2 = Arrays.stream(firstnames).map(String::toUpperCase).reduce("", (s1, s2) -> s1.concat(" " + s2));
+        System.out.println(result2);
+
+        String result3 = Arrays.stream(firstnames).map(String::toUpperCase).reduce((s1, s2) -> s1.concat(", " + s2)).orElse("");
+        System.out.println(result3);
+
+        String result4 = Arrays.stream(firstnames).collect(Collectors.joining(","));
+        System.out.println(result4);
+
+        int n = 65;
+        System.out.println("List of " + n + " random numbers");
+        System.out.println(StreamUtils.randomNumberList(n));
+
+        int start = 25;
+        int step = 5;
+        int size = 20;
+        System.out.println("List of " + size + " numbers from " + start + " with " + step + " increments.");
+        System.out.println(StreamUtils.orderedNumberList(start, step, size));
+    }
+}
