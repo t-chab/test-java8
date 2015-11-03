1a67f41485ceda2437789700ba96ccf936b442ce
tchabaud <tom@chabs.name>
2015-11-03 12:25:25 +0100

    Identation

--- src/com/java8/training/exercises/exercise2/Step4.java
+++ src/com/java8/training/exercises/exercise2/Step4.java
@@ -10,23 +10,23 @@ class Step4 {
 
     public static void main(String[] args) {
 
-    List<String> words = Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "blob");
+        List<String> words = Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "blob");
 
-    int size = 4;
-    List<String> shortWords = StringUtils.allMatches(words, s -> s.length() <= size);
-    System.out.println("Words shorter than " + size + ": " + shortWords);
+        int size = 4;
+        List<String> shortWords = StringUtils.allMatches(words, s -> s.length() <= size);
+        System.out.println("Words shorter than " + size + ": " + shortWords);
 
-    final String letter = "b";
-    List<String> wordsWithLetter = StringUtils.allMatches(words, s -> s.contains(letter));
-    System.out.println("Words that contains " + letter + ": " + wordsWithLetter);
+        final String letter = "b";
+        List<String> wordsWithLetter = StringUtils.allMatches(words, s -> s.contains(letter));
+        System.out.println("Words that contains " + letter + ": " + wordsWithLetter);
 
-    List<String> evenLengthWords = StringUtils.allMatches(words, s -> (s.length()
-            % 2) == 0);
-    System.out.println("Even length words : " + evenLengthWords);
+        List<String> evenLengthWords = StringUtils.allMatches(words, s -> (s.length()
+                % 2) == 0);
+        System.out.println("Even length words : " + evenLengthWords);
 
-    List<Integer> nums = Arrays.asList(1, 10, 100, 1000, 10000);
-    System.out.println("Nums : " + nums);
-    List<Integer> bigNums = ElementUtils.allMatches(nums, n -> n>500);
-    System.out.println("Big Nums : " + bigNums);
-}
+        List<Integer> nums = Arrays.asList(1, 10, 100, 1000, 10000);
+        System.out.println("Nums : " + nums);
+        List<Integer> bigNums = ElementUtils.allMatches(nums, n -> n > 500);
+        System.out.println("Big Nums : " + bigNums);
+    }
 }
