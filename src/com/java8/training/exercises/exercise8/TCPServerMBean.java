8a85673095672500d72e1eb2910f3220672d5eaf
tchabaud <tom@chabs.name>
2015-11-04 16:38:00 +0100

    Added comment.

--- src/com/java8/training/exercises/exercise8/TCPServerMBean.java
+++ src/com/java8/training/exercises/exercise8/TCPServerMBean.java
@@ -1,5 +1,7 @@
 package com.java8.training.exercises.exercise8;
 
+// Be careful : interface name should be the class suffixed by MBean
+// otherwise it won't work
 public interface TCPServerMBean {
 
     String getLabel();
